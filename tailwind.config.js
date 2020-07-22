712ba4f88e94756d71647b64f25e517b510c2944
Alan Hardman <alan@phpizza.com>
2020-07-22 15:19:15 -0600

    Adding Inter typeface
    This will use San Francisco over Inter on macOS/iOS since I like it better, but Inter is really close in metrics and style so I'm using it right after San Francisco in the stack so other operating systems will closely resemble the Apple experience.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Inter var',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
     container: {
       center: true,
       padding: '1rem',
