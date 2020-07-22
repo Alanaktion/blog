e30d3ea89e5d9841e3598d0ee0483f2a2d035cf6
Alan Hardman <alan@phpizza.com>
2020-07-22 11:02:40 -0600

    Adding fully-designed archive page
    Fixes #1
    
    It may still make sense to add more over time, something like a hero unit could add a lot to this page.

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -8,33 +8,64 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
+  const years = []
+  posts.forEach(({ node }) => {
+    const d = new Date(node.frontmatter.date)
+    const y = d.getFullYear()
+    if (years.indexOf(y) === -1) {
+      years.push(y)
+    }
+  })
+
+  const postsByYear = {}
+  posts.forEach(({ node }) => {
+    const d = new Date(node.frontmatter.date)
+    const y = d.getFullYear()
+    if (postsByYear[y] === undefined) {
+      postsByYear[y] = []
+    }
+    postsByYear[y].push(node)
+  })
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Archive" />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+
+      {years.map(year => {
         return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: '.25rem',
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
+          <div className="sm:flex my-8 md:my-12">
+            <h2 className="sm:mr-6 md:mr-8 lg:mr-12 sm:mb-4 text-3xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-4xl sm:leading-none md:text-5xl">
+              {year}
+            </h2>
+            <div className="flex-1 lg:max-w-3xl">
+              {postsByYear[year].map(node => {
+                return (
+                  <article
+                    key={node.fields.slug}
+                    className="mb-4 sm:mb-6"
+                  >
+                    <header className="mb-2">
+                      <h3 className="font-bold text-indigo-600 text-xl sm:text-2xl md:text-3xl">
+                        <Link className="hover:underline focus:underline" to={node.fields.slug}>
+                          {node.frontmatter.title}
+                        </Link>
+                      </h3>
+                      <p className="text-gray-700 text-sm -mt-1">
+                        {node.frontmatter.date}
+                      </p>
+                    </header>
+                    <section>
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: node.frontmatter.description || node.excerpt,
+                        }}
+                      />
+                    </section>
+                  </article>
+                )
+              })}
+            </div>
+          </div>
         )
       })}
     </Layout>
