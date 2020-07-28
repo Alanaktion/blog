07e17737307bbc1300af8cab5d25702fa40c304e
Alan Hardman <alan@phpizza.com>
2020-07-28 13:39:58 -0600

    Adding syntax highlighting for code blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 import "./src/css/index.css"
+import "prism-themes/themes/prism-nord.css"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,36 @@
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
+    fontFamily: {
+      mono: [
+        "JetBrains Mono",
+        "Fira Code",
+        ...fontFamily.mono,
+      ],
+    },
     extend: {},
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
     typography: {
       default: {
         css: {
           a: {
-            color: '#5a67d8',
+            color: "#5a67d8",
           },
-          'code::before': {
-            content: '',
+          "code::before": {
+            content: "",
           },
-          'code::after': {
-            content: '',
+          "code::after": {
+            content: "",
           },
         },
       },
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 }
