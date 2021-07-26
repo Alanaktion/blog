d3fc878522a806b5c5d5b4dcb7f5317d5ea0a237
Alan Hardman <alanaktion@gmail.com>
2021-07-25 22:46:29 -0600

    Make nav items a bit nicer
    Still need to handle mobile better in many ways but this fixes up desktop

--- src/components/search/search-result.js
+++ src/components/search/search-result.js
@@ -8,6 +8,7 @@ import {
   Snippet,
   PoweredBy,
 } from "react-instantsearch-dom"
+import { Transition } from "@headlessui/react"
 
 const HitCount = connectStateResults(({ searchResults }) => {
   const hitCount = searchResults && searchResults.nbHits
@@ -20,14 +21,12 @@ const HitCount = connectStateResults(({ searchResults }) => {
 })
 
 const PageHit = ({ hit }) => (
-  <div>
-    <Link to={hit.slug}>
-      <h4>
-        <Highlight attribute="title" hit={hit} tagName="mark" />
-      </h4>
-      <Snippet attribute="excerpt" hit={hit} tagName="mark" />
-    </Link>
-  </div>
+  <Link to={hit.slug} className="block px-4 py-2 hover:bg-gray-200 focus:bg-gray-200 bg-opacity-75 dark:hover:bg-purple-900 dark:focus:bg-purple-900">
+    <h4 className="font-bold text-gray-900 dark:text-gray-100">
+      <Highlight attribute="title" hit={hit} tagName="mark" />
+    </h4>
+    <Snippet className="dark:text-purple-300" attribute="excerpt" hit={hit} tagName="mark" />
+  </Link>
 )
 
 const HitsInIndex = ({ index }) => (
@@ -37,18 +36,25 @@ const HitsInIndex = ({ index }) => (
   </Index>
 )
 
+const dropdownClass = `overflow-auto origin-top absolute right-0 mt-2 max-w-md py-2 z-10 rounded-md shadow-lg bg-white dark:bg-purple-800 border dark:border-purple-600`
+
 const SearchResult = ({ indices, show }) => (
-  <div
-    style={{
-      display: show ? "block" : "none",
-    }}
-    className="SearchResults absolute overflow-auto z-10 right-0 mt-2 py-2 max-w-md bg-white border shadow sm:shadow-lg rounded-sm"
+  <Transition
+    show={Boolean(show)}
+    unmount={false}
+    className={`SearchResults ${dropdownClass}`}
+    enter="transition duration-100 ease-out"
+    enterFrom="transform scale-95 opacity-0"
+    enterTo="transform scale-100 opacity-100"
+    leave="transition duration-75 ease-out"
+    leaveFrom="transform scale-100 opacity-100"
+    leaveTo="transform scale-95 opacity-0"
   >
     {indices.map(index => (
       <HitsInIndex index={index} key={index.name} />
     ))}
     <PoweredBy />
-  </div>
+  </Transition>
 )
 
 export default SearchResult
--- src/components/theme-menu.js
+++ src/components/theme-menu.js
@@ -1,5 +1,5 @@
-import React, { useState, createRef, useEffect } from "react"
-import useClickOutside from "./search/use-click-outside"
+import React, { Fragment, useState, useEffect } from "react"
+import { Listbox, Transition } from "@headlessui/react"
 
 const themes = ['auto', 'light', 'dark']
 
@@ -25,14 +25,11 @@ const ThemeMenu = () => {
     setTheme(localStorage.theme || 'auto')
   }, [])
 
-  const innerRef = createRef()
-  useClickOutside(innerRef, () => setOpen(false))
-
-  const dropdownClass = `origin-top-right absolute right-0 mt-1 w-40 py-1 z-10 rounded-md shadow-lg bg-white dark:bg-purple-800 border dark:border-purple-600 ${open ? 'visible' : 'hidden'}`
+  const dropdownClass = `absolute right-0 mt-1 w-40 py-1 z-10 rounded-md shadow-lg bg-white dark:bg-purple-800 border dark:border-purple-600`
 
   return (
-    <div className="relative" ref={innerRef}>
-      <button
+    <Listbox as="div" className="relative" value={theme} onChange={applyTheme}>
+      <Listbox.Button
         type="button"
         className="ml-2 lg:ml-3 px-3 py-2 rounded-md text-sm font-medium leading-5 text-purple-100 hover:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none focus:text-yellow-300 focus:bg-purple-700 dark:focus:text-yellow-300 transition duration-150 ease-in-out"
         title="Toggle Dark Theme"
@@ -42,36 +39,45 @@ const ThemeMenu = () => {
         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
         </svg>
-      </button>
-      <div
-        className={dropdownClass}
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="theme-menu">
-        {themes.map(t => {
-          const active = t === theme;
-          const currentClasses = active ?
-            'text-white bg-yellow-500 hover:bg-yellow-600 bg-pan-30-smooth text-shadow' :
-            'text-gray-700 hover:bg-gray-100 dark:text-purple-200 dark:hover:bg-purple-700'
-          return (
-            <button
-              type="button"
-              className={`flex items-center appearance-none w-full px-4 py-2 text-sm focus:outline-none ${currentClasses}`}
-              onClick={() => { applyTheme(t) }} key={t}>
-              <span className="capitalize">{t}</span>
-              {theme === t && (
-                <div className="ml-auto">
-                  <span className="sr-only">(active)</span>
-                  <svg className={`w-4 h-4 ${active ? 'filter drop-shadow' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                </div>
+      </Listbox.Button>
+      <Transition
+        className="relative z-10 origin-top-right"
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Listbox.Options className={dropdownClass}>
+          {themes.map(t => (
+            <Listbox.Option key={t} value={t} as={Fragment}>
+              {({ active, selected }) => (
+                <li
+                  className={
+                    `flex items-center appearance-none w-full px-4 py-2 text-sm focus:outline-none ${
+                      selected ?
+                        `text-white ${active ? 'bg-yellow-600' : 'bg-yellow-500'} bg-pan-30-smooth text-shadow` :
+                        `${active ? 'bg-gray-100 dark:bg-purple-700' : ''} text-gray-700 dark:text-purple-200`
+                    }`
+                  }
+                >
+                  <span className="capitalize">{t}</span>
+                  {selected &&
+                    <div className="ml-auto">
+                      <span className="sr-only">(active)</span>
+                      <svg className={`w-4 h-4 ${active ? 'filter drop-shadow' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      </svg>
+                    </div>
+                  }
+                </li>
               )}
-            </button>
-          )
-        })}
-      </div>
-    </div>
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Transition>
+    </Listbox>
   )
 }
 
