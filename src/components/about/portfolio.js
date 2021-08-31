9826f7ab455409f04e55ae29192d4da76d4a8013
Alan Hardman <alan@phpizza.com>
2021-08-31 12:52:44 -0600

    Updating portfolio styling + new image sources

--- src/components/about/portfolio.js
+++ src/components/about/portfolio.js
@@ -6,7 +6,16 @@ const PortfolioImage = p => {
   if (typeof p.img === 'string') {
     return (
       <img src={p.img} alt={p.alt}
-        className="rounded-sm mb-4 shadow"
+        className="rounded-sm mb-4 border dark:border-gray-700 shadow"
+        loading="lazy"
+      />
+    )
+  }
+  if (typeof p.img === 'object' && p.img.srcset) {
+    return (
+      <img src={p.img.src} alt={p.alt}
+        srcSet={p.img.srcset}
+        className="rounded-sm mb-4 border dark:border-gray-700 shadow"
         loading="lazy"
       />
     )
@@ -16,13 +25,13 @@ const PortfolioImage = p => {
       <img
         src={p.img.light}
         alt={p.alt}
-        className="block dark:hidden rounded-sm shadow"
+        className="block dark:hidden rounded-sm border dark:border-gray-700 shadow"
         loading="lazy"
       />
       <img
         src={p.img.dark}
         alt={p.alt}
-        className="hidden dark:block rounded-sm shadow"
+        className="hidden dark:block rounded-sm border dark:border-gray-700 shadow"
         loading="lazy"
       />
     </div>
@@ -35,11 +44,11 @@ const Portfolio = (props) => {
       <div className="prose lg:prose-lg xl:prose-xl max-w-none mb-4">
         <p>Here are a few of the things I've designed and built:</p>
       </div>
-      <ul className="-mx-4 overflow-x-scroll flex w-screen sm:overflow-x-auto sm:mx-0 sm:px-4 sm:w-full sm:grid grid-cols-2 lg:grid-cols-3 sm:gap-4 lg:gap-6 dark:text-gray-50">
+      <ul className="-mx-4 overflow-x-scroll flex w-screen sm:overflow-x-auto sm:mx-0 sm:w-full sm:grid grid-cols-2 lg:grid-cols-3 sm:gap-4 lg:gap-6 dark:text-gray-50">
         {items.map(p => (
           <li className="relative mx-4 sm:mx-0 w-48 mb-2 sm:mb-0 sm:w-auto flex-shrink-0 flex flex-col" key={p.title}>
             {p.img && PortfolioImage(p)}
-            <a href={p.href} className="text-lg lg:text-xl font-semibold text-gray-700 dark:text-gray-200 group">
+            <a href={p.href} target="_blank" className="text-lg lg:text-xl font-semibold text-gray-700 dark:text-gray-200 group">
               {p.title}
               <span className="absolute inset-0 group-focus:ring ring-offset-4 ring-purple-400 rounded-sm" />
             </a>
