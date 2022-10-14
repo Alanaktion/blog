e32872b817afa7c5407504633d4b307829959bf1
Alan Hardman <alanaktion@gmail.com>
2022-10-14 10:45:36 -0600

    Only Latin fonts for now
    If Gatsby wasn't inlining I wouldn't care as much but yeah

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-import "@fontsource/yusei-magic"
-import "@fontsource/inter/variable.css"
+import "@fontsource/yusei-magic/latin.css"
+// import "@fontsource/inter/variable.css" // Manually added in index.css to limit to Latin charset
 import "./src/css/index.css"
 import "./src/css/prism-fairyfloss.css"
