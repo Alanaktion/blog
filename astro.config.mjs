97f8cb3f5fd996c0d174bdb98e0e54da425f5428
Alan Hardman <alanaktion@gmail.com>
2025-09-11 22:54:30 +0000

    Netlify image CDN?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.phpizza.com',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   prefetch: {
     prefetchAll: true
   },
+
+  adapter: netlify(),
 });
